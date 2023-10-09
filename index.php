39eff17522ef15d59707686028103312a827accf
Pablo Sepulveda <pablosep@msn.com>
2023-10-08 20:35:07 -0700

    Build My Portal Page

---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MyPortal page</title>
+    <link href="css/styles.css" type="text/css" rel="stylesheet">
+</head>
+<body>
+    
+<h1><a href="index.php">Pablo's Portal Page</a></h1>
+<h2>The navigation below represents our Big Assignments.</h2>
+
+<div id="wrapper">
+    <nav>
+        <ul>
+            <li><a href="">Switch</a></li>
+            <li><a href="">Troubleshoot</a></li>
+            <li><a href="">Calculator</a></li>
+            <li><a href="">Email</a></li>
+            <li><a href="">Database</a></li>
+            <li><a href="">Gallery</a></li>
+
+        </ul>
+    </nav>
+
+    <main>
+    <h2>About Pablo</h2>
+    <img class="pic" src="images/puppy_face.jpg" alt="Face">
+    <p>Hello there. Here I am again, slowly learning how to 
+        make websites. I am continuing where i left off, after 
+        learning some. Since I am learning slowly it is
+        taking me a long time to learn. My goal is to learn enough web development to make a working website for myself, and my business ideas. My hobby is to learn Website development. I also enjoy learning ASL language and working out at the gym. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laudantium nesciunt necessitatibus earum in reiciendis voluptate pariatur neque aperiam. Harum cupiditate accusamus ipsa aspernatur. Quia sit, soluta fugiat totam sint ipsa.</p>
+
+        <img class="pic" src="images/mamps_page.jpg" alt="mamps screenshot">
+
+    </main>
+
+    <aside>
+<h2>Weekly Class Exercises</h2>
+<h3>Week2</h3>
+<ol>
+    <li><a href="var.php">var.php</li>
+    <li><a href="var.php">var.php</li>
+    <li><a href="var.php">var.php</li>
+    <li><a href="var.php">var.php</li>
+    <li><a href="var.php">var.php</li>
+
+<ol>
+
+    </aside>
+</div>
+<!-- close wrapper -->
+</body>
+</html>
