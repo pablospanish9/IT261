a5d492531841e167924d2ca23533ce2bed15d05a
Pablo Sepulveda <pablosep@msn.com>
2023-10-09 20:40:14 -0700

    more portal page and first var

---
+++ weeks/week2/var.php
@@ -0,0 +1,38 @@
+<?php
+// my variables
+// variable is a container for data
+
+$name = "Pablo"; //Pablo is a string
+echo $name;
+
+$body_temp = 98; //integer
+$body_temp_new = 98.6; //float
+echo '<br>';
+echo $body_temp;
+echo '<br>';
+echo $body_temp_new;
+echo '<br>';
+echo "My name is $name!";
+echo '<br>';
+echo 'My name is $name!';
+
+echo '<br>';
+echo 'My name is '.$name.'!';
+
+echo '<br>';
+echo 'The normal body temperature for a human being is '.$body_temp_new.'';
+echo '<br>';
+$name = "Sharon"; //Pablo is a string
+echo $name;
+$x = 20;
+$y = 5;
+echo '<br>';
+$z = $x + $y;
+echo $z;
+echo '<br>';
+
+
+echo '<br>';
+
+
+echo '<br>';
